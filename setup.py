cabdab7d9aa1f4a4d6877e22f0ccb807fefdf3f8
Brian Rosner <brosner@gmail.com>
2010-01-12 23:01:14 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+
+
+setup(
+    name = "django-oauth-access",
+    version = "0.1.dev1",
+    author = "Eldarion",
+    author_email = "development@eldarion.com",
+    description = "centralized oAuth access to oAuth providers in Django",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    url = "http://github.com/eldarion/django-oauth-access",
+    packages = [
+        "oauth_access",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
