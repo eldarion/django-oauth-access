8d7980a04243fa079e0b23cf8cdcfce45e803ecd
Brian Rosner <brosner@gmail.com>
2010-08-06 00:12:54 -0600

    added LoginCallback class to handle boilerplate performed in many callbacks which authenticate users from another service

---
+++ oauth_access/callback.py
@@ -0,0 +1,35 @@
+from django.shortcuts import redirect
+
+
+class LoginCallback(object):
+    
+    def __call__(self, request, access, token):
+        if not request.user.is_authenticated():
+            user_data = self.fetch_user_data(request, access, token)
+            user = self.lookup_user(request, access, user_data)
+            if user is None:
+                request.session["oauth_signup_data"] = {
+                    "token": token,
+                    "user_data": user_data,
+                }
+                return redirect(
+                    reverse(
+                        "oauth_access_finish_signup", kwargs={
+                            "service": access.service
+                        }
+                    )
+                )
+            else:
+                user.backend = "django.contrib.auth.backends.ModelBackend"
+                login(request, user)
+        else:
+            user = request.user
+        redirect_to = self.redirect_url()
+        access.persist(user, token)
+        return redirect(redirect_to)
+    
+    def redirect_url(self, request):
+        raise NotImplementedError()
+    
+    def lookup_user(self, request, access, user_data):
+        return access.lookup_user(identifier=self.identifier_from_data(user_data))
