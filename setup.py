2143eb9bbfde9b5ffa56741e23bc13ce8c98c1e5
Brian Rosner <brosner@gmail.com>
2012-07-12 16:37:28 -0600

    Updated setup.py for dev branch

--- setup.py
+++ setup.py
@@ -4,17 +4,15 @@ from distutils.core import setup
 
 setup(
     name = "django-oauth-access",
-    version = "0.1.dev22",
+    version = "1.0a1.dev1",
     author = "Eldarion",
     author_email = "development@eldarion.com",
-    description = "centralized oAuth access to oAuth providers in Django",
+    description = "centralized OAuth access to OAuth providers in Django",
     long_description = open("README.rst").read(),
     license = "BSD",
     url = "http://github.com/eldarion/django-oauth-access",
     packages = [
         "oauth_access",
-        "oauth_access.templatetags",
-        "oauth_access.utils",
     ],
     classifiers = [
         "Development Status :: 3 - Alpha",
